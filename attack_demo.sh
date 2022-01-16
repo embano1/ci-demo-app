faf7a3ee51770aa1929ced4327bbba9c5094b2d0
houdini91 <mdstrauss91@gmail.com>
2022-01-16 08:34:51 +0200

    test

--- attack_demo.sh
+++ attack_demo.sh
@@ -4,7 +4,9 @@ echo "Enter next tag"
 read tag
 echo -e "" >> go.sum
 git add go.sum
-git commit -m "attack,sign,verify"
-git tag -a $tag -m "attack,sign,verify"
+# MESSAGE="attack,sign,verify"
+MESSAGE="attack"
+git commit -m $MESSAGE
+git tag -a $tag -m $MESSAGE
 git push origin main
 git push --tags
